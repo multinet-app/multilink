f48c6126398fa89065bb0ae8862b71119c726d1a
JackWilb <jackwilburn@tutanota.com>
2019-09-18 14:06:52 -0600

    pull from AWS

--- src/js/multinet.js
+++ src/js/multinet.js
@@ -9,18 +9,18 @@ let graph_structure;
 
 async function load_data(workspace, graph) {
     // Fetch the node and edge tables
-    tables_call = "http://127.0.0.1:8080/api/workspaces/" + workspace + "/graphs/" + graph
+    tables_call = "http://multinet.app/api/workspaces/" + workspace + "/graphs/" + graph
     await load_tables(tables_call)
 
     // Loop through each node table and fetch the nodes to global variables
     for (node_table of tables.nodeTables) {
-        nodes_call = "http://127.0.0.1:8080/api/workspaces/" + workspace + "/tables/" + node_table
+        nodes_call = "http://multinet.app/api/workspaces/" + workspace + "/tables/" + node_table
         await load_nodes(nodes_call)
     }
 
     // Load the edge table to a global variable
     edge_table = tables.edgeTable
-    links_call = "http://127.0.0.1:8080/api/workspaces/" + workspace + "/tables/" + edge_table
+    links_call = "http://multinet.app/api/workspaces/" + workspace + "/tables/" + edge_table
     await load_links(links_call)
 
     // Set the global graph structure
