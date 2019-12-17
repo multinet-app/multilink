68340c2c7077d02be620ffd9c9e4fdeb36ea2610
Roni Choudhury <roni.choudhury@kitware.com>
2019-12-17 14:20:55 -0500

    Add publicPath to vue configuration

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
+const process = require('process');
+
+const publicPath = process.env.PUBLIC_PATH || '/';
+
 module.exports = {
   transpileDependencies: [
     "vuetify"
-  ]
-}
+  ],
+  publicPath: process.env.NODE_ENV === 'production' ? publicPath : '/',
+};
