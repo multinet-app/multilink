fd9afbbe73ce0770fbd9cf33931bfee550219769
JackWilb <jackwilburn@tutanota.com>
2019-12-20 13:52:42 -0700

    add a basic test to test functionality

---
+++ src/tests/unit/control_panel.spec.js
@@ -0,0 +1,16 @@
+import { shallowMount } from '@vue/test-utils'
+import Controls from '../../App.vue'
+
+describe("Control Panel", () => {
+    describe("searchFor", () => {
+        it("Searching for node in empty list throws error", () => {
+            // Arrange
+
+            // Act
+            
+            //Assert
+            expect(1).toEqual(1)
+
+        });
+    });
+});
