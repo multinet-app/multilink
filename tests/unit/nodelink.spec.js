08bb5728bedc65009e972825c021f6c8f8180fb3
JackWilb <jackwilburn@tutanota.com>
2019-12-30 11:48:34 -0700

    update the test to remove all errors and to flesh out the arc path tests

--- tests/unit/nodelink.spec.js
+++ tests/unit/nodelink.spec.js
@@ -1,5 +1,6 @@
-import { createLocalVue, shallowMount } from '@vue/test-utils'
+import { createLocalVue, shallowMount, mount } from '@vue/test-utils'
 import Vuetify from 'vuetify/lib';
+import NodeLink from '@/components/NodeLink/NodeLink.vue';
 import App from '@/App.vue';
 
 describe("Node-Link Utils", () => {
@@ -7,26 +8,52 @@ describe("Node-Link Utils", () => {
         let wrapper;
 
         beforeEach(()=>{
-            const localVue = createLocalVue()
-            localVue.use(Vuetify)
+            wrapper = mount(NodeLink, {
+                propsData: { 
+                    graphStructure: {nodes: [], links: [{ source: {"x": 100, "y": 150}, target: {"x": 400, "y": 450} }]},
+                    app: App,
+                    provenance: {addObserver: () => {}},
+                }
+            })
 
-            wrapper = shallowMount(App, {
-            localVue
-            });
+            // Set some computed properties
+            wrapper.vm.browser = {
+                height: 1000,
+                width: 1000
+            }
+            wrapper.vm.panelDimensions = { width: 250, height: 1000 }
+            wrapper.vm.visDimensions = { width: 750, height: 1000 }
         })
 
         it("A straight arc returns the expected path", () => {
             // Arrange
-            let link = {}
-            link.source = {"x": 100, "y": 150}
-            link.target = {"x": 200, "y": 250}
-            console.log(wrapper.contains('#app'))
+            wrapper.vm.straightEdges = true
+            let link = wrapper.vm.graphStructure.links[0]
 
             // Act
-            // let arc = wrapper.arcPath(false, link, false)
+            let arc = wrapper.vm.arcPath(true, link, false)
             
             // Assert
-            // expect(arc).toEqual(`M ${link.source.x} ${link.source.y} L ${link.target.x} ${link.target.y}`)
+            expect(arc).toEqual(`M ${link.source.x + wrapper.vm.nodeMarkerLength / 2} ${link.source.y + wrapper.vm.nodeMarkerHeight / 2} L ${link.target.x + wrapper.vm.nodeMarkerLength / 2} ${link.target.y + wrapper.vm.nodeMarkerHeight / 2}`)
+        });
+
+        it("A curved arc returns the expected path", () => {
+            // Arrange
+            wrapper.vm.straightEdges = false
+            let link = wrapper.vm.graphStructure.links[0]
+            const dx = link.source.x - link.target.x
+            const dy = link.source.y - link.target.y
+            const dr = Math.sqrt(dx * dx + dy * dy)
+            const xRotation = 0
+            const largeArc = 0
+            const sweep = 1
+
+            // Act
+            let arc = wrapper.vm.arcPath(true, link, false)
+            console.log(arc)
+            
+            // Assert
+            expect(arc).toEqual(`M ${link.source.x + wrapper.vm.nodeMarkerLength / 2}, ${link.source.y + wrapper.vm.nodeMarkerHeight / 2} A ${dr}, ${dr} ${xRotation}, ${largeArc}, ${sweep} ${link.target.x + wrapper.vm.nodeMarkerLength / 2},${link.target.y + wrapper.vm.nodeMarkerHeight / 2}`)
         });
     });
 });
