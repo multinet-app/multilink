76d6c540c8017beef2c2ac86b9639bf42bcaa365
JackWilb97 <jackwilburn@tutanota.com>
2020-03-19 14:47:17 -0600

    Expand the logic to link variables and fix the y axis clipping

--- src/components/NodeLink/Legend.vue
+++ src/components/NodeLink/Legend.vue
@@ -81,7 +81,7 @@ export default {
 
   data() {
     return {
-      svgHeight: 100
+      svgHeight: 150
     };
   },
 
@@ -128,60 +128,65 @@ export default {
 
   methods: {
     setUpPanel() {
-      for (const nodeAttr of this.multiVariableList) {
-        // Get the SVG element and its width
-        const variableSvg = d3.select(`#node${nodeAttr}`)
-        const variableSvgWidth = variableSvg.node().getBoundingClientRect().width
+      // For node and link variables
+      for (const list of [this.multiVariableList, this.linkVariableList]) {
+        // For each attribute
+        for (const attr of list) {
+          // Get the SVG element and its width
+          const type = list === this.multiVariableList ? "node" : "link"
+          const variableSvg = d3.select(`#${type}${attr}`)
+          const variableSvgWidth = variableSvg.node().getBoundingClientRect().width
 
-        // Get the data and generate the bins
-        const currentData = this.graphStructure.nodes.map(d => d[nodeAttr])
-        const bins = currentData.reduce((prev, curr) => (prev[curr] = ++prev[curr] || 1, prev), {})
-        const binLabels = Object.entries(bins).map(d => d[0])
-        const binValues = Object.entries(bins).map(d => d[1])
+          // Get the data and generate the bins
+          const currentData = this.graphStructure[`${type}s`].map(d => d[attr])
+          const bins = currentData.reduce((prev, curr) => (prev[curr] = ++prev[curr] || 1, prev), {})
+          const binLabels = Object.entries(bins).map(d => d[0])
+          const binValues = Object.entries(bins).map(d => d[1])
 
-        // Generate axis scales
-        const yScale = d3.scaleLinear()
-          .range([this.svgHeight, 0])
-          .domain([d3.min(binValues), d3.max(binValues)]);
+          // Generate axis scales
+          const yScale = d3.scaleLinear()
+            .range([this.svgHeight, 0])
+            .domain([d3.min(binValues), d3.max(binValues)]);
 
-        const xScale = d3.scaleBand()
-          .range([0, variableSvgWidth])
-          .domain(binLabels);
+          const xScale = d3.scaleBand()
+            .range([10, variableSvgWidth])
+            .domain(binLabels);
 
-        // Add the axis scales onto the chart
-        variableSvg
-          .append('g')
-          .call(d3.axisLeft(yScale));
+          // Add the axis scales onto the chart
+          variableSvg
+            .append('g')
+            .attr('transform', `translate(10,0)`)
+            .call(d3.axisLeft(yScale));
 
-        variableSvg
-          .append('g')
-          .attr('transform', `translate(0, ${this.svgHeight})`)
-          .call(d3.axisBottom(xScale));
+          variableSvg
+            .append('g')
+            .attr('transform', `translate(10, ${this.svgHeight})`)
+            .call(d3.axisBottom(xScale));
 
-        // Add the bars
-        const variableSvgEnter = variableSvg
-          .selectAll()
-          .data(currentData)
-          .enter()
-          .append('rect')
-          .attr('x', (d) => xScale(d))
-          .attr('y', (d) => yScale(bins[d]))
-          .attr('height', (d, i, values) => this.svgHeight - yScale(bins[d]))
-          .attr('width', xScale.bandwidth())
-          .attr('fill', this.isQuantitative(nodeAttr) ? this.nodeColorScale(d) : '#82B1FF');
+          // Add the bars
+          const variableSvgEnter = variableSvg
+            .selectAll()
+            .data(currentData)
+            .enter()
+            .append('rect')
+            .attr('x', (d) => xScale(d))
+            .attr('y', (d) => yScale(bins[d]))
+            .attr('height', (d, i, values) => this.svgHeight - yScale(bins[d]))
+            .attr('width', xScale.bandwidth())
+            .attr('fill', this.isQuantitative(attr) ? this.nodeColorScale(d) : '#82B1FF');
 
-        // Add the brush
-        variableSvg
-          .call(
-            d3.brushX()
-              .extent([[0, 0], [variableSvgWidth, this.svgHeight]])
-              .on("start brush", () => {
-                const extent = d3.event.selection;
-                console.log(extent)
-                variableSvgEnter
-                  .attr("stroke", d => xScale(d) >= extent[0] - xScale.bandwidth() && xScale(d) <= extent[1] ? "#000000" : "")
-              })
-          );
+          // Add the brush
+          variableSvg
+            .call(
+              d3.brushX()
+                .extent([[0, 0], [variableSvgWidth, this.svgHeight]])
+                .on("start brush", () => {
+                  const extent = d3.event.selection;
+                  variableSvgEnter
+                    .attr("stroke", d => xScale(d) >= extent[0] - xScale.bandwidth() && xScale(d) <= extent[1] ? "#000000" : "")
+                })
+            );
+        }
       }
     },
 
@@ -196,14 +201,14 @@ export default {
   <div>
     <v-card>
       <v-list disabled>
-        <v-list-item-group>
+        <v-list-item-group disabled>
           <v-subheader>Node Attributes</v-subheader>
           <v-list-item
-            v-for="(nodeAttr, index) of this.multiVariableList"
+            v-for="nodeAttr of this.multiVariableList"
             :key="'node' + nodeAttr"
           >
-            <v-list-item-content>
-              <v-list-item-title v-text="nodeAttr + index"></v-list-item-title>
+            <v-list-item-content disabled>
+              <v-list-item-title v-text="nodeAttr"></v-list-item-title>
               <br/>
 
               <svg :id="'node' + nodeAttr" :height="svgHeight + 20"/>
@@ -212,11 +217,11 @@ export default {
 
           <v-subheader>Link Attributes</v-subheader>
           <v-list-item
-            v-for="(linkAttr, index) of this.linkVariableList"
+            v-for="linkAttr of this.linkVariableList"
             :key="'link' + linkAttr"
           >
-            <v-list-item-content>
-              <v-list-item-title v-text="linkAttr + index"></v-list-item-title>
+            <v-list-item-content disabled>
+              <v-list-item-title v-text="linkAttr"></v-list-item-title>
               <br/>
               <svg :id="'link' + linkAttr" :height="svgHeight + 20"/>
             </v-list-item-content>
