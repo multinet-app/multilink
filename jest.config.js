80d58f13d94eb85695dfc3656d18a4e416a00dcc
Roni Choudhury <roni.choudhury@kitware.com>
2020-06-24 17:37:11 -0400

    Extend jest config to work with typescript codebase

--- jest.config.js
+++ jest.config.js
@@ -1,13 +1,24 @@
 module.exports = {
     transformIgnorePatterns: ['/node_modules/(?!vuetify)'],
-    moduleFileExtensions: ['js', 'jsx', 'json', 'vue'],
+    moduleFileExtensions: ['js', 'jsx', 'json', 'vue', 'ts'],
+
+    moduleNameMapper: {
+      '^@/(.*)$': '<rootDir>/src/$1'
+    },
 
     transform: {
     '^.+\\.vue$': 'vue-jest',
     '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
         'jest-transform-stub',
+    '^.+\\.tsx?$': 'ts-jest',
     '^.+\\.(js|jsx)?$': 'babel-jest'
     },
 
-    preset: '@vue/cli-plugin-unit-jest'
+    preset: '@vue/cli-plugin-unit-jest',
+
+    globals: {
+      'ts-jest': {
+        tsConfig: "tsconfig.spec.json"
+      }
+    }
 }
