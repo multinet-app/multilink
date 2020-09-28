301d5b1ba5b3ca2b29d8d69df039c5666528bb6a
Jack Wilburn <jackwilburn@tutanota.com>
2020-09-28 13:47:48 -0600

    Add .eslintrc.js and move old eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  root: true,
+
+  env: {
+    node: true,
+  },
+
+  extends: [
+    'plugin:vue/recommended',
+    '@vue/airbnb',
+    '@vue/typescript',
+    'plugin:vue/recommended',
+    '@vue/typescript/recommended',
+  ],
+
+  plugins: [
+    'vuetify',
+  ],
+
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'max-len': ['off'],
+    'import/prefer-default-export': ['off'],
+  },
+
+  parserOptions: {
+    parser: '@typescript-eslint/parser',
+    ecmaVersion: 2020,
+    ecmaFeatures: {
+      modules: true,
+    },
+  },
+};
