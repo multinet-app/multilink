8c695bfa0d3d2b9bd5f8599aae821d474dcc6548
JackWilb <jackwilburn@tutanota.com>
2020-11-09 11:24:41 -0700

    Mount multilink in App.vue

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@ import store from '@/store';
 import { getUrlVars } from '@/lib/utils';
 
 import Controls from './components/Controls.vue';
+import MultiLink from './components/MultiLink/MultiLink.vue';
 
 export default {
   name: 'App',
 
   components: {
     Controls,
+    MultiLink,
   },
 
   computed: {
@@ -34,9 +36,28 @@ export default {
 
 <template>
   <v-app>
-    <v-content>
-      <controls />
-    </v-content>
+    <v-main>
+      <v-row>
+        <v-col cols="3">
+          <controls />
+        </v-col>
+
+        <v-col
+          ref="multilink_container"
+          cols="9"
+        >
+          <v-row>
+            <multi-link
+              v-if="network && selectedNodes"
+              :svg-dimensions="{
+                width: this.$refs.multilink_container.clientWidth,
+                height: this.$refs.multilink_container.clientHeight,
+              }"
+            />
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-main>
   </v-app>
 </template>
 
