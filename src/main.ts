dbb54c262b8970377581a1d6a0c4f6527629ea95
JackWilb <jackwilburn@tutanota.com>
2020-12-23 09:42:30 -0700

    Add composition API dependency

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 import Vue from 'vue';
 import App from '@/App.vue';
+import VueCompositionApi from '@vue/composition-api';
 import vuetify from '@/plugins/vuetify';
 
 Vue.config.productionTip = false;
+
+Vue.use(VueCompositionApi);
 
 new Vue({
   vuetify,
