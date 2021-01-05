61632e3904785b3bcaab15f76c5404944ab8ad06
JackWilb <jackwilburn@tutanota.com>
2021-01-05 09:59:50 -0700

    Add Blinker font

--- src/App.vue
+++ src/App.vue
@@ -86,7 +86,7 @@ export default {
 
 <style>
 #app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  font-family: "Blinker", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   overflow: none;
