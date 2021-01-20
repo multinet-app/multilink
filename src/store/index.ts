4c6a3f494e87332a92db6d43ad8e17ad9814e7c2
JackWilb <jackwilburn@tutanota.com>
2021-01-20 12:51:14 -0700

    Add undo/redo event listener for provenance

--- src/store/index.ts
+++ src/store/index.ts
@@ -428,6 +428,23 @@ const {
       );
 
       storeState.provenance.done();
+
+      // Add keydown listener for undo/redo
+      document.addEventListener('keydown', (event) => {
+        if (
+          (event.ctrlKey && event.code === 'KeyZ' && !event.shiftKey) // ctrl + z (no shift)
+          || (event.metaKey && event.code === 'KeyZ' && !event.shiftKey) // meta + z (no shift)
+        ) {
+          storeState.provenance.undo();
+        } else if (
+          (event.ctrlKey && event.code === 'KeyY') // ctrl + y
+          || (event.ctrlKey && event.code === 'KeyZ' && event.shiftKey) // ctrl + shift + z
+          || (event.metaKey && event.code === 'KeyY') // meta + y
+          || (event.metaKey && event.code === 'KeyZ' && event.shiftKey) // meta + shift + z
+        ) {
+          storeState.provenance.redo();
+        }
+      });
     },
   },
 });
