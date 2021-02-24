6dc7008e0b8332cc872cbc9177e399562f3f01d7
JackWilb <jackwilburn@tutanota.com>
2021-02-23 18:37:50 -0700

    Add function + UI to un-pin selected nodes

--- src/components/ContextMenu.vue
+++ src/components/ContextMenu.vue
@@ -25,10 +25,24 @@ export default {
       }
     }
 
+    function unPinSelectedNodes() {
+      if (network.value !== null) {
+        network.value.nodes
+          .filter((node) => selectedNodes.value.has(node._id))
+          .forEach((node) => {
+            // eslint-disable-next-line no-param-reassign
+            delete node.fx;
+            // eslint-disable-next-line no-param-reassign
+            delete node.fy;
+          });
+      }
+    }
+
     return {
       rightClickMenu,
       clearSelection,
       pinSelectedNodes,
+      unPinSelectedNodes,
     };
   },
 };
@@ -59,6 +73,15 @@ export default {
         >
           <v-list-item-content>
             <v-list-item-title>Pin Selected Nodes</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item
+          dense
+          @click="unPinSelectedNodes()"
+        >
+          <v-list-item-content>
+            <v-list-item-title>Un-Pin Selected Nodes</v-list-item-title>
           </v-list-item-content>
         </v-list-item>
       </v-list>
