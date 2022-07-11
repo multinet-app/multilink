9a4a2a9179f0478240815b8150fbbc67e0b6c77f
JackWilb <jackwilburn@tutanota.com>
2022-07-11 13:16:38 -0600

    Port app.vue to script setup

--- src/App.vue
+++ src/App.vue
@@ -1,64 +1,29 @@
-<script lang="ts">
+<script setup lang="ts">
 import store from '@/store';
 import { getUrlVars } from '@/lib/utils';
-import {
-  ref, computed, Ref,
-} from '@vue/composition-api';
+import { computed } from 'vue';
 
 import AlertBanner from '@/components/AlertBanner.vue';
 import ControlPanel from '@/components/ControlPanel.vue';
 import MultiLink from '@/components/MultiLink.vue';
 import ProvVis from '@/components/ProvVis.vue';
 
-export default {
-  name: 'App',
+const urlVars = getUrlVars(); // Takes workspacce and network
+store.dispatch.fetchNetwork({
+  workspaceName: urlVars.workspace,
+  networkName: urlVars.network,
+}).then(() => {
+  store.dispatch.createProvenance();
+  store.dispatch.guessLabel();
+});
 
-  components: {
-    AlertBanner,
-    ControlPanel,
-    MultiLink,
-    ProvVis,
-  },
+const network = computed(() => store.state.network);
+const selectedNodes = computed(() => store.state.selectedNodes);
+const loadError = computed(() => store.state.loadError);
 
-  setup() {
-    const network = computed(() => store.state.network);
-    const selectedNodes = computed(() => store.state.selectedNodes);
-    const loadError = computed(() => store.state.loadError);
+// Provenance vis boolean
+const showProvenanceVis = computed(() => store.state.showProvenanceVis);
 
-    const multilinkContainer: Ref<Element | null> = ref(null);
-    const multilinkContainerDimensions = computed(() => {
-      if (multilinkContainer.value !== null) {
-        return {
-          width: multilinkContainer.value.clientWidth - 24,
-          height: multilinkContainer.value.clientHeight - 24,
-        };
-      }
-      return null;
-    });
-
-    const urlVars = getUrlVars(); // Takes workspacce and network
-
-    store.dispatch.fetchNetwork({
-      workspaceName: urlVars.workspace,
-      networkName: urlVars.network,
-    }).then(() => {
-      store.dispatch.createProvenance();
-      store.dispatch.guessLabel();
-    });
-
-    // Provenance vis boolean
-    const showProvenanceVis = computed(() => store.state.showProvenanceVis);
-
-    return {
-      network,
-      selectedNodes,
-      loadError,
-      multilinkContainer,
-      multilinkContainerDimensions,
-      showProvenanceVis,
-    };
-  },
-};
 </script>
 
 <template>
