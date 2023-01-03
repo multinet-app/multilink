9ddf7c9f8e668afd749ead216f7acc7cfa9a8495
Jack Wilburn <jackwilburn@tutanota.com>
2023-01-03 12:00:29 -0700

    Add function to compare states + get the changes, remove old function

--- src/lib/provenanceUtils.ts
+++ src/lib/provenanceUtils.ts
@@ -1,56 +1,35 @@
-import { createAction } from '@visdesignlab/trrack';
-import { ProvenanceEventTypes, State } from '@/types';
+import { ProvState } from '@/types';
 
-export function updateProvenanceState(piniaState: State, label: ProvenanceEventTypes) {
-  const stateUpdateActions = createAction<State, State[], ProvenanceEventTypes>((provState, newProvState) => {
-    if (label === 'Select Node(s)' || label === 'De-select Node' || label === 'Clear Selection') {
-      provState.selectedNodes = newProvState.selectedNodes;
-    } else if (label === 'Set Display Charts') {
-      provState.displayCharts = newProvState.displayCharts;
-    } else if (label === 'Set Marker Size') {
-      provState.markerSize = newProvState.markerSize;
-    } else if (label === 'Set Font Size') {
-      provState.fontSize = newProvState.fontSize;
-    } else if (label === 'Set Label Variable') {
-      provState.labelVariable = newProvState.labelVariable;
-    } else if (label === 'Set Node Color Variable') {
-      provState.nodeColorVariable = newProvState.nodeColorVariable;
-    } else if (label === 'Set Node Size Variable') {
-      provState.nodeSizeVariable = newProvState.nodeSizeVariable;
-    } else if (label === 'Set Select Neighbors') {
-      provState.selectNeighbors = newProvState.selectNeighbors;
-    } else if (label === 'Set Directional Edges') {
-      provState.directionalEdges = newProvState.directionalEdges;
-    } else if (label === 'Set Edge Length') {
-      provState.edgeLength = newProvState.edgeLength;
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export function undoRedoKeyHandler(event: KeyboardEvent, provenance: any) {
+  if (
+    (event.ctrlKey && event.key === 'z' && !event.shiftKey) // ctrl + z (no shift)
+    || (event.metaKey && event.key === 'z' && !event.shiftKey) // meta + z (no shift)
+  ) {
+    if (provenance.current.id !== provenance.root.id) {
+      provenance.undo();
     }
-  })
-    .setLabel(label);
-
-  if (piniaState.provenance !== null) {
-    piniaState.provenance.apply(stateUpdateActions(piniaState));
+  } else if (
+    (event.ctrlKey && event.key === 'y') // ctrl + y
+    || (event.ctrlKey && event.key === 'z' && event.shiftKey) // ctrl + shift + z
+    || (event.metaKey && event.key === 'y') // meta + y
+    || (event.metaKey && event.key === 'z' && event.shiftKey) // meta + shift + z
+  ) {
+    if (provenance.current.children.length > 0) {
+      provenance.redo();
+    }
   }
 }
 
-export function undoRedoKeyHandler(event: KeyboardEvent, storeState: State) {
-  // If provenance doesn't exist, exit
-  if (storeState.provenance == null) { return; }
+// Order of the objects matter! The second object is treated as the updated version of the first object
+export function findDifferencesInPrimitiveStates(firstObj: ProvState, secondObj: ProvState) {
+  const updates: Partial<ProvState> = {};
 
-  if (
-    (event.ctrlKey && event.code === 'KeyZ' && !event.shiftKey) // ctrl + z (no shift)
-    || (event.metaKey && event.code === 'KeyZ' && !event.shiftKey) // meta + z (no shift)
-  ) {
-    if (storeState.provenance.current.id !== storeState.provenance.root.id) {
-      storeState.provenance.undo();
+  Object.entries(secondObj).forEach(([key, value]) => {
+    if (firstObj[key as keyof ProvState] !== secondObj[key as keyof ProvState]) {
+      updates[key as keyof ProvState] = value;
     }
-  } else if (
-    (event.ctrlKey && event.code === 'KeyY') // ctrl + y
-    || (event.ctrlKey && event.code === 'KeyZ' && event.shiftKey) // ctrl + shift + z
-    || (event.metaKey && event.code === 'KeyY') // meta + y
-    || (event.metaKey && event.code === 'KeyZ' && event.shiftKey) // meta + shift + z
-  ) {
-    if (storeState.provenance.current.children.length > 0) {
-      storeState.provenance.redo();
-    }
-  }
+  });
+
+  return updates;
 }
