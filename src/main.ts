9ee6afc77cf89097b5f11d51dc2fee7f28e6c510
Jack Wilburn <jackwilburn@tutanota.com>
2023-03-14 14:47:42 -0600

    Fix issues with invalidating cookies and restoring logins

--- src/main.ts
+++ src/main.ts
@@ -5,25 +5,32 @@ import api from '@/api';
 import oauthClient from '@/oauth';
 import { oauthClientId, prodBuild } from '@/environment';
 import { createPinia, PiniaVuePlugin } from 'pinia';
-import { readSharedLoginCookie } from 'multinet';
+import { invalidateSharedLoginCookie, readSharedLoginCookie, writeSharedLoginCookie } from 'multinet';
 
 Vue.config.productionTip = false;
-
 Vue.use(PiniaVuePlugin);
 const pinia = createPinia();
 
-const key = `oauth-token-${oauthClientId}`;
+const loginTokenKey = `oauth-token-${oauthClientId}`;
 const sharedLoginCookie = readSharedLoginCookie();
-if (prodBuild) {
-  if (sharedLoginCookie) {
-    localStorage.setItem(key, sharedLoginCookie);
-  } else {
-    localStorage.removeItem(key);
-  }
+if (prodBuild && sharedLoginCookie !== null) {
+  localStorage.setItem(loginTokenKey, sharedLoginCookie);
 }
 
 oauthClient.maybeRestoreLogin().then(() => {
   Object.assign(api.axios.defaults.headers.common, oauthClient.authHeaders);
+
+  // If logged out, remove the local storage item
+  if (!Object.keys(oauthClient.authHeaders).includes('Authorization')) {
+    localStorage.removeItem(loginTokenKey);
+  }
+
+  const tokenString = localStorage.getItem(loginTokenKey);
+  if (tokenString !== null) {
+    writeSharedLoginCookie(tokenString);
+  } else {
+    invalidateSharedLoginCookie();
+  }
 
   new Vue({
     vuetify,
